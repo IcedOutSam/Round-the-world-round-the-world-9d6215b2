d7e8497feeab354c943af8c4ec3f61b141bad238
Sam Ahmed <2102926@talnet.nl>
2020-03-31 14:27:13 +0200

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,12 +1,11 @@
-
- <?php
+<?php
 $Doekoe           = array(
-    '50',
-    '20',
-    '10',
-    '5',
-    '2',
-    '1'
+   '50',
+   '20',
+   '10',
+   '5',
+   '2',
+   '1'
 );
 $aantal           = $argv[1];
 $Bruh             = intval($aantal);
@@ -15,30 +14,30 @@ $aantal           = round($aantal += 0.02, 2, PHP_ROUND_HALF_UP);
 $compleetCentjes  = ($aantal - $Bruh) * 100;
 $restantenCentjes = $compleetCentjes;
 foreach ($Doekoe as $value) {
-    if ($aantal > 0) {
-        $totaal = floor($restanten / $value);
-        if ($totaal > 0) {
-            echo "U krijgt $totaal x €$value " . PHP_EOL;
-            $aantal    = $restanten;
-            $restanten = floor($restanten % $value);
-        }
-    }
+   if ($aantal > 0) {
+       $totaal = floor($restanten / $value);
+       if ($totaal > 0) {
+           echo "U krijgt $totaal x €$value " . PHP_EOL;
+           $aantal    = $restanten;
+           $restanten = floor($restanten % $value);
+       }
+   }
 }
 $centjes = array(
-    '50',
-    '20',
-    '10',
-    '5'
+   '50',
+   '20',
+   '10',
+   '5'
 );
 foreach ($centjes as $value2) {
-    if ($compleetCentjes > 0) {
-        $totaalCentjes = floor($restantenCentjes / $value2);
-        if ($totaalCentjes > 0) {
-            echo "U krijgt $totaalCentjes x $value2 cent" . PHP_EOL;
-            $compleetCentjes  = $restantenCentjes;
-            $restantenCentjes = ($restantenCentjes % $value2);
-        }
-    }
+   if ($compleetCentjes > 0) {
+       $totaalCentjes = floor($restantenCentjes / $value2);
+       if ($totaalCentjes > 0) {
+           echo "U krijgt $totaalCentjes x $value2 cent" . PHP_EOL;
+           $compleetCentjes  = $restantenCentjes;
+           $restantenCentjes = ($restantenCentjes % $value2);
+       }
+   }
 }
 
 
